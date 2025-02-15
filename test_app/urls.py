f0fc96421fc29bbf5adc2728771e352cf70b391f
Krd2024 <polzov.ya@ya.ru>
2025-02-15 14:42:59 +0300

    26

--- test_app/urls.py
+++ test_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", main, name="main"),
     path("tests/", tests, name="tests"),
     path("work/", work, name="work"),
-    path("api/v1/tests/", TestViewSet.as_view()),
+    path("api/v1/devices/", TestViewSet.as_view()),
     path("add_devices/", add_devices, name="add_devices"),
 ]
